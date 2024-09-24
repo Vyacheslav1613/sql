608ddfbf3f44bbb8f1d446195893e6188f5e1c43
Вячеслав <ldobryakl@yandex.ru>
2024-09-24 15:55:18 +0500

    add repository

--- src/main/java/ru/netology/sql/ProductService.java
+++ src/main/java/ru/netology/sql/ProductService.java
@@ -1,42 +1,22 @@
 package ru.netology.sql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.netology.sql.repository.SqlRepository;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    public List<String> fetchProductsByName(String name) throws SQLException {
-        List<String> products = new ArrayList<>();
+    @Autowired
+    private SqlRepository sqlRepository;
 
-        try (var connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/postgres", // Проверьте имя базы данных
-                "postgres",
-                "123");
-             var statement = connection.prepareStatement(
-                     "SELECT A.product_name " +
-                             "FROM netology.orders A " +
-                             "JOIN netology.customers B ON A.customer_id = B.id " +
-                             "WHERE LOWER(B.name) = LOWER(?);")) {
-            statement.setString(1, name);
-
-            var resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                products.add(resultSet.getString("product_name"));
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        }
-        if (products.isEmpty()) {
-            return Collections.singletonList(name + " в списках покупателей не найден");
-        }
-        return products;
+    @Transactional
+    public List<String> findProductsByName(String name) throws SQLException {
+        String jpqlQuery = "SELECT c.name FROM Customers c WHERE LOWER(c.name) = LOWER(:name)";
+        return sqlRepository.fetchProductsByName(name);
     }
 }
